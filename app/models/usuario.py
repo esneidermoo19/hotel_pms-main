d28de0955449c6c05534d9393ed20fa8d0731d1a
esneidermoo19 <esneidermoo19@gmail.com>
2026-05-05 07:03:54 -0500

    Update usuario.py

--- app/models/usuario.py
+++ app/models/usuario.py
@@ -6,7 +6,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column('password', db.String(128), nullable=False)
+    password_hash = db.Column('password', db.String(256), nullable=False)
     nombre = db.Column(db.String(100))
     email = db.Column(db.String(120))
     telefono = db.Column(db.String(20))
